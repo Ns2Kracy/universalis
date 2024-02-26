35d2505b9e4bcd2eca3c6b4cad2fad5aaa0ed320
Ns2Kracy <ns2kracy@gmail.com>
2024-02-26 23:14:42 +0800

    WIP v2 api

--- src/api/v2/mod.rs
+++ src/api/v2/mod.rs
@@ -1,1 +1,16 @@
+pub mod content;
+pub mod data_centers;
 pub mod entities;
+pub mod least_recently_updated;
+pub mod market_board_current_data;
+pub mod market_board_sale_history;
+pub mod marketable;
+pub mod most_recently_updated;
+pub mod recently_updated;
+pub mod tax_rates;
+pub mod trade_volume;
+pub mod upload_history;
+pub mod uploader_upload_counts;
+pub mod user_lists;
+pub mod world_upload_counts;
+pub mod worlds;
